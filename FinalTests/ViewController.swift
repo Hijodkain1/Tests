7ba5d8c7b96bcbd30b33024d44dd7826e68fdb77
Juan carlos <juancarlos@MacBook-Pro-de-Juanca.local>
2017-02-11 02:07:26 +0100

    Creación de los ficheros de la app

---
+++ FinalTests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinalTests
+//
+//  Created by Juan carlos on 11/2/17.
+//  Copyright © 2017 Juan carlos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
